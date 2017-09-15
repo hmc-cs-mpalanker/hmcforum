c5d96c0e479b8dbff3351f671fdccfb0a19f20cd
hmc-cs-mpalanker <mpalanker@cs.hmc.edu>
2017-09-14 20:31:36 -0700

    Generated new methods for controllers

--- forum/config/routes.rb
+++ forum/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+
+  get 'groups/new'
+
+  get 'groups/index'
+
+  get 'groups/show'
+
+  get 'posts/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
