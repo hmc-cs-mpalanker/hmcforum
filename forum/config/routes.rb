06d5ca99333dccc64b4e502273b5203e74b62d05
hmc-cs-eamason <eamason@cs.hmc.edu>
2017-09-17 16:40:20 -0700

    Revert "I continued to work on the nav bar."
    This reverts commit 389eee3abcdad21017909a403291e445b36dcd13.

--- forum/config/routes.rb
+++ forum/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  root 'discussions#index'
-  resources :posts 
-  devise_for :users
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
