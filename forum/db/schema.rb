036bd69fff0532373d3e4128a662affa1cb5817b
hmc-cs-mpalanker <mpalanker@cs.hmc.edu>
2017-09-22 00:20:32 -0700

    Profile Pictures!

--- forum/db/schema.rb
+++ forum/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918031251) do
+ActiveRecord::Schema.define(version: 20170922070203) do
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20170918031251) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
